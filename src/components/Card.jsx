39bc811a9d57c8d9bd200f22ba693a3b4bfae340
ByeBach <byebachgm@gmail.com>
2024-10-27 19:59:33 -0300

    img contain in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,18 @@ import styles from './Card.module.css';
 
 const Card = ({ character: { name, race, ki, maxKi, gender, description, affiliation, image } }) => { 
 
-
-  
   return(
     <>
     
     <div className={styles.card}>
-      <img src={image} alt={name}  />
+      <div className={styles.cardimg}>
+        <img  src={image} alt={name}  />
+      </div>
       <h2>{name}</h2>
       <p><strong>Race:</strong> {race}</p>
-      <p><strong>Gender:</strong> {gender}</p>
       <p><strong>Ki:</strong> {ki}</p>
       <p><strong>Max Ki:</strong> {maxKi}</p>
       <p><strong>Affiliation:</strong> {affiliation}</p>
-      <p >{description}</p>
     </div>
     </>
   )
